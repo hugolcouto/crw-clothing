63a9c1d96bc14a2eb9226a1a948c25376ab0d7f8
hugolcouto <hugocouto.dev@gmail.com>
2019-09-18 16:17:42 -0300

    Secure commit

---
+++ src/utils/firebase.js
@@ -0,0 +1,26 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+var config = {
+    apiKey: "AIzaSyDeoPC7qMmSxnKbhMKaxgxLCZszY5XL62U",
+    authDomain: "crwn-db-4e78e.firebaseapp.com",
+    databaseURL: "https://crwn-db-4e78e.firebaseio.com",
+    projectId: "crwn-db-4e78e",
+    storageBucket: "",
+    messagingSenderId: "412383175715",
+    appId: "1:412383175715:web:325c4f2bad01577c433d96"
+};
+
+firebase.initializeApp(config);
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+const provider = new firebase.auth.GoogleAuthProvider()
+
+provider.setCustomParameters({ prompt: 'select_account' });
+
+export const SignInWithGoogle  = () => auth.signInWithPopup();
+
+export default firebase;
