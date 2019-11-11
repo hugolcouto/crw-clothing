b32782b9e5787140fb991e8aef04a9db9eb0bf97
Hugo Couto <hugo.couto@webhub.com.br>
2019-11-11 15:23:24 -0300

    Added checkout page, checkout item component and updated cart to use selector from reselect

--- src/components/checkout/Checkout.jsx
+++ src/components/checkout/Checkout.jsx
@@ -3,6 +3,7 @@ import './Checkout.scss';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 import { selectCartItems, selectCartTotal } from '../../redux/cart/cart.selectors';
+import CheckoutItem from '../checkout-item/Checkout-item';
 
 const Checkout = ({ cartItems, total }) => (
     <div className="checkout-page">
@@ -13,7 +14,7 @@ const Checkout = ({ cartItems, total }) => (
             <div className="header-block"><span>Valor</span></div>
             <div className="header-block"><span>Remover</span></div>
         </div>
-        { cartItems.map(cartItem => cartItem.name) }
+        { cartItems.map(cartItem => <CheckoutItem cartItem={cartItem} key={cartItem.id} />) }
         <div className="total"><span>TOTAL: ${total}</span></div>
     </div>
 );
