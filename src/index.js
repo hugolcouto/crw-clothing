64c54bbf58c859e5d5a6b0dc2ca66b7da1722aca
Hugo Couto <hugo.couto@webhub.com.br>
2019-11-12 16:06:45 -0300

    Added persist storage redux function with redux-persist

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import store from './redux/store';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import { store, persistor } from './redux/store'
 
 import App from './App';
 
@@ -11,7 +12,9 @@ ReactDOM.render(
 
     <Provider store={store}>
         <BrowserRouter>
-            <App/>
+            <PersistGate persistor={persistor}>
+                <App/>
+            </PersistGate>
         </BrowserRouter>
     </Provider>
 
